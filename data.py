eb95df55d2afca56549fb1b490dc7a8179bd5636
BROTHERHOODOP <108978489+BROTHERHOODOP@users.noreply.github.com>
2022-08-14 06:40:00 +0530

    Update data.py

--- data.py
+++ data.py
@@ -9,7 +9,7 @@ class Data:
     buttons = [
         generate_single_button,
         [InlineKeyboardButton("❣️ sᴜᴩᴩᴏʀᴛ ❣️", url="https://t.me/THE_BROTHERHOOD_COUNCIL"),
-         InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", url="https://t.me/THE_BROTHERHOOD_COUNCIL_SUPPORT"),
+         InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", url="https://t.me/TBH_COUNCIL_SUPPORT"),
         ],
     ]
 
@@ -20,5 +20,5 @@ Tʜɪs ɪs {},
 Aɴ ᴏᴩᴇɴ sᴏᴜʀᴄᴇᴅ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
 
 Sᴏᴜʀᴄᴇ : [SUPPORT](https://t.me/THE_BROTHERHOOD_COUNCIL)
-Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [BROTHERHOOD](https://t.me/THE_BROTHERHOOD_COUNCIL_SUPPORT) !
+Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [BROTHERHOOD](https://t.me/TBH_COUNCIL_SUPPORT) !
     """
